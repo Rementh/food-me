a725ebfe7f3c8361e72c3608164eefdf1f81004c
Filip Talaga <talaga.filip@gmail.com>
2019-05-02 21:58:34 +0200

    Added sample data fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from './firebase';
 
-const title = 'Food me';
+class App extends Component {
+    state = {
+        data: null
+    };
 
-const App = () => <h2>{title}</h2>;
+    componentDidMount() {
+        const user = {
+            email: 'test@test.com',
+            password: 'testtest'
+        };
+
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .then(this.loadData)
+            .catch(console.log);
+    }
+
+    loadData = () => {
+        firebase.database().ref('recipes').once('value').then(snap =>
+            this.setState({ data: snap.val() })
+        );
+    }
+
+    render = () =>
+        <React.Fragment>
+            {this.state.data &&
+                this.state.data.map((recipe, index) =>
+                    <p key={index}>{recipe.title}</p>
+                )}
+        </React.Fragment>;
+}
 
 export default App;
