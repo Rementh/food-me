43ebc101b11c3c77de514cfcf2ed7effd6cd4406
Filip Talaga <talaga.filip@gmail.com>
2019-09-24 13:23:25 +0200

    Added firestore

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 module.exports = {
     apiKey: 'AIzaSyAlcLI7fLatHN_VUoBccI68FHjOLSm5vO0',
     authDomain: 'https://food-me-app.firebasapp.com/',
-    databaseURL: 'https://food-me-app.firebaseio.com/'
+    databaseURL: 'https://food-me-app.firebaseio.com/',
+    projectId: 'food-me-app'
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 import * as config from '../firebase-config';
 
 const makeFirebaseInstance = () => {
     app.initializeApp(config);
 
     const auth = app.auth();
+    const firestore = app.firestore();
 
     const restFunctions = {
         get: (query, refresh = false) => auth.currentUser.getIdToken(refresh).then(idToken =>
@@ -29,9 +31,15 @@ const makeFirebaseInstance = () => {
         isInitialized: () => new Promise(resolve => auth.onAuthStateChanged(resolve))
     };
 
-    const recipesFunctions = {
+    const deprecatedRecipesFunctions = {
         getRecipes: () => get('recipes'),
         getRecipe: id => get('recipes').then(data => data[id]),
+        addRecipe: recipe => put(`recipes/${recipe.id}`, recipe.body).then(() => get('recipes', true).then(() => recipe.id))
+    };
+
+    const recipesFunctions = {
+        getRecipes: () => firestore.collection('recipes').get().then(snap => snap.docs.map(doc => ({ id: doc.id, data: doc.data() }))),
+        getRecipe: id => firestore.collection('recipes').doc(id).get().then(snap => ({ id: snap.id, data: snap.data() })),
         addRecipe: recipe => put(`recipes/${recipe.id}`, recipe.body).then(() => get('recipes', true).then(() => recipe.id))
     };
 
